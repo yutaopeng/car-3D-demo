c2fa1d99f70b8130d9da462f53057ef87f0791d3
yutaopeng <yutaopeng@gmail.com>
2023-01-15 19:06:53 -0800

    update the light

--- src/script.js
+++ src/script.js
@@ -5,50 +5,46 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 // init
 
 const camera = new THREE.PerspectiveCamera(
-  40,
+  35,
   window.innerWidth / window.innerHeight,
-  1,
-  5000
+  0.1,
+  1000
 );
-camera.position.z = 1;
+
+camera.position.set(0, 5, 30);
+// camera.position.z = 1;
 // camera.rotation.y = (45 / 180) * Math.PI;
 // camera.position.x = 800;
 // camera.position.y = 100;
 // camera.position.z = 1000;
 
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0xdddddd);
+// scene.background = new THREE.Color(0xdddddd);
 
-const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
-const material = new THREE.MeshNormalMaterial();
-
-const mesh = new THREE.Mesh(geometry, material);
-// scene.add(mesh);
-
-const hlight = new THREE.AmbientLight(0x404040, 100);
+const hlight = new THREE.AmbientLight(0x404040, 10);
 scene.add(hlight);
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 100);
-directionalLight.position.set(0, 1, 0);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 5);
+directionalLight.position.set(-50, 50, 100);
 directionalLight.castShadow = true;
 scene.add(directionalLight);
 
-const light = new THREE.PointLight(0xc4c4c4, 10);
+const light = new THREE.PointLight(0xc4c4c4, 2);
 light.position.set(0, 300, 500);
 scene.add(light);
-const light2 = new THREE.PointLight(0xc4c4c4, 10);
+const light2 = new THREE.PointLight(0xc4c4c4, 2);
 light2.position.set(500, 100, 0);
 scene.add(light2);
-const light3 = new THREE.PointLight(0xc4c4c4, 10);
+const light3 = new THREE.PointLight(0xc4c4c4, 2);
 light3.position.set(0, 100, -500);
 scene.add(light3);
-const light4 = new THREE.PointLight(0xc4c4c4, 10);
+const light4 = new THREE.PointLight(0xc4c4c4, 2);
 light4.position.set(-500, 300, 500);
 scene.add(light4);
 
-const renderer = new THREE.WebGLRenderer({ antialias: true });
+const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
-// renderer.setAnimationLoop(animation);
+renderer.setPixelRatio(window.devicePixelRatio);
 document.body.appendChild(renderer.domElement);
 
 const controls = new OrbitControls(camera, renderer.domElement);
@@ -60,7 +56,7 @@ loader.load(
   "car/audi.glb",
   function (gltf) {
     const car = gltf.scene.children[0];
-    car.scale.set(0.03, 0.03, 0.03);
+    car.scale.set(0.5, 0.5, 0.5);
     scene.add(gltf.scene);
     animate();
   },
@@ -79,3 +75,12 @@ function animate() {
 
   renderer.render(scene, camera);
 }
+
+function onWindowResize() {
+  camera.aspect = container.clientWidth / container.clientHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(container.clientWidth, container.clientHeight);
+}
+
+window.addEventListener("resize", onWindowResize);
